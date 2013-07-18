0a13baada6582a117ba3322b98c3125c13bbde43
Brett Sanders <brettrsanders@gmail.com>
2013-07-18 00:02:58 -0500

    Url model shortens urls, view notifications, move stuff to layout

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,18 @@
 class Url < ActiveRecord::Base
-  # Remember to create a migration!
   belongs_to :user
+  before_create :shorten_url
+  validates :full_url, format: { with: /http[s]?:\/\/[\w\.]*/ },
+                       presence: true
+
+  private
+
+    def random_short_url
+      short_url = ""
+      6.times { short_url += rand(100).to_s }
+    end
+
+    def shorten_url
+      self.short_url = random_short_url
+    end
+
 end
